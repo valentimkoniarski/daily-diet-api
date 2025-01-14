e5ba84db8a9f08e0d238738ad3fbbb50d22231a7
valentimkoniarski <valentim.koniarski@hotmail.com>
2025-01-14 18:06:50 -0300

    fix: set server host to '0.0.0.0' for external accessibility

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { app } from './app'
 import { env } from './env'
 
 app.listen({
+  host: '0.0.0.0',
   port: env.PORT,
 }).then(() => {
   console.log(`Server is running on http://localhost:${env.PORT}`)
