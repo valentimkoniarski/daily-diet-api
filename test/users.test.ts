169cb0a83003c4acf6ef13e58318054fc4b36b14
valentimkoniarski <valentim.koniarski@hotmail.com>
2025-01-14 20:28:10 -0300

    test: add user routes tests using vitest and supertest

---
+++ test/users.test.ts
@@ -0,0 +1,31 @@
+import { it, expect, beforeAll, afterAll, describe, beforeEach } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+import { execSync } from 'node:child_process'
+
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+beforeEach(() => {
+  execSync('npm run knex migrate:rollback --all')
+  execSync('npm run knex migrate:latest')
+})
+
+describe('Testes das rotas de users', () => {
+  it('O usuário deve conseguir se cadastrar', async () => {
+    const response = await request(app.server).post('/users').send({
+      username: 'Valentim Koniarski Junior',
+    })
+
+    const cookies = response.get('Set-Cookie')
+
+    expect(cookies).toEqual(
+      expect.arrayContaining([expect.stringContaining('sessionId')]),
+    )
+  })
+})
